e43bc015684f0dbc17f4d7a95ad12f88e90da9eb
amartuvshin <educatornum@gmail.com>
2020-11-19 14:16:14 +0900

    next

--- 2) Вэб хэрхэн ажилладаг вэ? POSTMAN ашиглан http мэссэжүүдийг харах.js
+++ 2) Вэб хэрхэн ажилладаг вэ? POSTMAN ашиглан http мэссэжүүдийг харах.js
@@ -1,2 +1,6 @@
 // HTTP => Hyper Text Transfer Protocol
 // DNS resolver => IP haygiig butsaaj ugnu
+
+
+// content-type 
+// cookies
---
+++ 3) http протокол түүний хувилбар, ажиллах зарчим, мэссэж, төрөл, статус кодын тайлбар.js
@@ -0,0 +1,9 @@
+/*
+
+GET    => ugugdul awna
+POST   => servert ugugdul ilgeene
+PUT    => server deerh ugudliig uurchilnu replace hiine
+PATCH  => server deer baiga ugugdliig uurchilnu (hesgiign uurchilnu)
+DELETE => server deeree ugudul ustgana
+
+*/
