81f037a9fadfec5184237dbe6c3374052e18c93a
amartuvshin <educatornum@gmail.com>
2020-11-19 14:26:48 +0900

    next

--- 3) http протокол түүний хувилбар, ажиллах зарчим, мэссэж, төрөл, статус кодын тайлбар.js
+++ 3) http протокол түүний хувилбар, ажиллах зарчим, мэссэж, төрөл, статус кодын тайлбар.js
@@ -7,3 +7,47 @@ PATCH  => server deer baiga ugugdliig uurchilnu (hesgiign uurchilnu)
 DELETE => server deeree ugudul ustgana
 
 */
+
+
+/*
+REQUEST 
+=======
+1. Authrization     ==> Login hiigdsen token baidag
+2. User-agent       ==> Hen ilgeesen ymar programaas ilgeesen uildliin system
+3. Referrer         ==> Ymar huudasnaas shiljilj irsen
+4. Accept           ==> Iim turul l huleej awahaa zaana
+5. Content-type     ==> Content type
+6. Content-lenght   ==> Content urd
+7. Cookies          ==> Cookies baina
+*/
+
+/*
+RESPONSE
+=======
+1. Content-type     ==> Content type
+2. Content-lenght   ==> Content urd
+3. Set-cookies      ==> iim ymiig cookiendee hadgal ==> medal zuuh gej oilgoj bolno
+4. Date             ==> udur ognoo
+5. Server           ==> Nginx, CentOS
+*/
+
+/*
+STATUS CODE
+=======
+server-ees client tal-d ugnu
+
+1XX => Huselt huleej awlaa
+2XX => Amjilttai bolson
+3XX => Redirect hiih utga
+4XX => Clinet taliin aldaa
+5XX => Server taliin aldaa
+
+200 -> OK
+201 -> OK hadgalchlaa
+301 -> URL uurchlugdsun
+304 -> Content uurchlugduugui heweerei baina
+400 -> tanii huseltiig bi oilgohgui baina
+401 -> Ta ehleed login hiine uu
+404 -> Tanii hussen zuil end alga
+500 -> Serveriin code buruu ajilsan tohioldold
+*/
